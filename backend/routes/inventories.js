3f2205c762028a37472dd7e25765100b69791b23
Davit <kheoshvili.dato@gmail.com>
2023-05-03 12:36:18 -0400

    Add routes

---
+++ backend/routes/inventories.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express();
+
+router
+    .get("/", (req, res) => {
+        res.status(200).json({ message: "inventory get for show data" });
+    })
+    .post("/", (req, res) => {
+        res.status(200).json({ message: "inventory post for create data" });
+    })
+
+router.delete("/:id", (req, res) => {
+    console.log(req.params.id);
+    res.status(200).json({ message: `inventory delete. user with id=${req.params.id}` });
+})
+
+// router.param("param", (req, res, next, param) => {
+// //     I will use this code to sort data later
+// })
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,17 @@
 const express = require("express");
 const app = express();
 
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "main page" });
+})
+
+const inventoriesRouter = require("./routes/inventories.js");
+
+app.use("/inventories", inventoriesRouter)
+
+app.use((req, res, next) => {
+    res.status(404).send("Sorry, can't find that!");
+});
+
 app.listen(3000);
