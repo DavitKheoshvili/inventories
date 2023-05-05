717e7040f87d00292f2d101115bdeb31ad93f803
Davit <kheoshvili.dato@gmail.com>
2023-05-05 08:07:25 -0400

    Implement routing Create AddPageComponent Add AddPageComponent and its path in routing array Test it

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddPageComponent } from './components/add-page/add-page.component'
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'add', component: AddPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/src/app/components/add-page/add-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'add-page',
+  templateUrl: './add-page.component.html',
+  styleUrls: ['./add-page.component.css']
+})
+export class AddPageComponent {
+
+}
